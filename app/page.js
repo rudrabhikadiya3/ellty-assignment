d8660ec7e58c3265f6550be640323cf4f30117d9
Rudra Bhikadiya <irudradev@gmail.com>
2025-03-29 14:01:27 +0530

    structre with basic style

--- app/page.js
+++ app/page.js
@@ -1,7 +1,45 @@
 export default function Home() {
   return (
-    <div className='center-container'>
-      <p>All Pages</p>
+    <div className='centerContainer'>
+      <div className='selectionBox'>
+        <ul className='selectionBoxContent'>
+          <li>
+            <span>All Pages</span>
+            <span>
+              <input type='checkbox' name='' id='' />
+            </span>
+          </li>
+          <hr />
+
+          <li>
+            <span>Page 1</span>
+            <span>
+              <input type='checkbox' name='' id='' />
+            </span>
+          </li>
+          <li>
+            <span>Page 1</span>
+            <span>
+              <input type='checkbox' name='' id='' />
+            </span>
+          </li>
+          <li>
+            <span>Page 1</span>
+            <span>
+              <input type='checkbox' name='' id='' />
+            </span>
+          </li>
+          <li>
+            <span>Page 1</span>
+            <span>
+              <input type='checkbox' name='' id='' />
+            </span>
+          </li>
+        </ul>
+        <hr />
+
+        <button>Done</button>
+      </div>
     </div>
   )
 }
